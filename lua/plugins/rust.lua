86cf41d40d502b4e20cea288921a331df579cd6b
messier1296 <hayato1048576@gmail.com>
2026-04-13 23:39:32 +0900

    Disable Rust codelens to avoid input corruption

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -3,5 +3,42 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^6",
     ft = { "rust" },
+    init = function()
+      ---@type rustaceanvim.Opts
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(client, bufnr)
+            -- rust-analyzer codelens updates appear to interfere with insert-mode
+            -- input in this environment, so keep Rust LSP/completion active but
+            -- disable codelens for Rust buffers.
+            client.server_capabilities.codeLensProvider = nil
+            if vim.lsp.codelens and vim.lsp.codelens.enable then vim.lsp.codelens.enable(false, { bufnr = bufnr }) end
+          end,
+          default_settings = {
+            ["rust-analyzer"] = {
+              check = {
+                command = "clippy",
+              },
+              cargo = {
+                allFeatures = true,
+              },
+              inlayHints = {
+                typeHints = {
+                  enable = true,
+                },
+                parameterHints = {
+                  enable = true,
+                },
+                chainingHints = {
+                  enable = true,
+                },
+              },
+            },
+          },
+        },
+      }
+
+      vim.lsp.inlay_hint.enable(true)
+    end,
   },
 }
