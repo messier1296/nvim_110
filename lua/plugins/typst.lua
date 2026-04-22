e85a01216935040a7fa65b66bd3976c337a1e5a8
messier1296 <hayato1048576@gmail.com>
2026-04-22 11:13:31 +0900

    fix:typstのrootディレクトリの設定を変更

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -2,23 +2,48 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig.util")
+      local lspconfig = require "lspconfig"
+      local util = require "lspconfig.util"
+      local function resolve_typst_root(fname)
+        return util.root_pattern("typst.toml", ".git", ".codex")(fname)
+          or vim.fs.dirname(vim.fn.fnamemodify(fname, ":p"))
+      end
 
-      lspconfig.tinymist.setup({
-        root_dir = util.root_pattern(".git", "main.typ"),
+      lspconfig.tinymist.setup {
+        root_dir = resolve_typst_root,
+        on_new_config = function(new_config, root_dir)
+          new_config.settings = new_config.settings or {}
+          new_config.settings.tinymist = vim.tbl_deep_extend("force", new_config.settings.tinymist or {}, {
+            rootPath = root_dir,
+          })
+        end,
         settings = {
-          formatterMode = "typstyle",
-          exportPdf = "onSave",
+          tinymist = {
+            formatterMode = "typstyle",
+            exportPdf = "onSave",
+          },
         },
-      })
+      }
     end,
   },
 
   {
     "chomosuke/typst-preview.nvim",
     ft = "typst",
-    opts = {},
+    opts = function()
+      local util = require "lspconfig.util"
+      local function resolve_typst_root(path)
+        return util.root_pattern("typst.toml", ".git", ".codex")(path)
+          or vim.fs.dirname(vim.fn.fnamemodify(path, ":p"))
+      end
+
+      return {
+        dependencies_bin = {
+          tinymist = "tinymist",
+        },
+        get_root = resolve_typst_root,
+      }
+    end,
     config = function(_, opts)
       require("typst-preview").setup(opts)
 
