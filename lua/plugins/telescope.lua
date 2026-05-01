87cf5dd46f64ad8b3bd84780545352db231aa55b
messier1296 <hayato1048576@gmail.com>
2026-05-01 11:45:28 +0900

    telescopeを追加

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    cmd = "Telescope",
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Help tags" },
+    },
+    opts = {},
+  },
+}
