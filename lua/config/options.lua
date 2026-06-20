fe612c9dfe81bc7acae962836da26181fd5262d3
messier1296 <hayato1048576@gmail.com>
2026-06-21 00:58:16 +0900

    Fix WSL clipboard provider

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,20 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.mouse = "a"
+if vim.fn.has "wsl" == 1 and vim.fn.executable "win32yank.exe" == 1 then
+  vim.g.clipboard = {
+    name = "win32yank",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
 vim.opt.clipboard = "unnamedplus"
 vim.opt.wrap = true
 vim.opt.fileformats = { "unix", "dos", "mac" }
